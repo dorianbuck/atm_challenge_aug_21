7fe2a22481cc16bc8711f19667a698d63f6905bf
johnnysee <js.johnny.see@gmail.com>
2021-08-30 21:00:23 +0200

    added the instance double with new tests, but the test gave some error messages - need to fix errors in atm.rb

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,23 @@
 require_relative '../src/atm.rb'
 
 describe Atm do
+  let(:account) { instance_double('Account') }
+
+  before do
+    # Before each test we need to add an attribute of `balance`
+    # to the `account` object and set the value to `100`
+    allow(account).to receive(:balance).and_return(100)
+    # We also need to allow `account` to receive the new balance
+    # using the setter method `balance=`
+    allow(account).to receive(:balance=)
+  end
+  it 'allow withdraw if the account has enough balance.' do
+
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
   it 'is expected to have 1000 on initialize' do
     expect(subject.funds).to eq 1000
   end
