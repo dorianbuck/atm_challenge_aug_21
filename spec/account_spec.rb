e7968688dba6e6c71b8e6df22da23233b9d3823e
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-01 16:56:18 +0200

    added expiry date, test and logic

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,4 +13,9 @@ describe Account do
   it 'is expected to have an owner' do
     expect(subject.owner).to eq person
   end
+
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
 end
--- src/account.rb
+++ src/account.rb
@@ -1,9 +1,16 @@
 class Account
-  attr_accessor :pin_code, :owner
-  
+  require 'date'
+  STANDARD_VALIDITY_YRS = 5
+  attr_accessor :pin_code, :owner, :exp_date
+
   def initialize(attrs = {})
     @pin_code = gen_pin_code
+    @exp_date = set_expire_date #Not positive about this one...
     set_owner(attrs[:owner])
+  end
+
+  def set_expire_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
   def gen_pin_code
@@ -11,7 +18,8 @@ class Account
   end
 
   private
+
   def set_owner(obj)
-    obj == nil ? missing_owner : @owner = obj
-end
+    obj.nil? ? missing_owner : @owner = obj
+  end
 end
