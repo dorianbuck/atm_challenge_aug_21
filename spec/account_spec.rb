992a0dd5d4a96c53d26cda7c6c867e3ee66ed6d7
johnnysee <js.johnny.see@gmail.com>
2021-09-03 12:57:27 +0200

    fixed all the errors in atm_spec - all spec files are now green

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,29 +1,29 @@
-require_relative '../src/account'
-require 'date'
+require_relative "../src/account"
+require "date"
 
 describe Account do
-  let(:person) { instance_double('person', name: 'Johnny') }
+  let(:person) { instance_double("person", name: "Johnny") }
   subject { described_class.new({ owner: person }) }
 
-  it 'is expected to have a 4 digit pincode' do
+  it "is expected to have a 4 digit pincode" do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
   end
 
-  it 'is expected to have an owner' do
+  it "is expected to have an owner" do
     expect(subject.owner).to eq person
   end
 
-  it 'is expected to raise error if no owner is set' do
-    expect { described_class.new }.to raise_error 'An Account owner is required'
+  it "is expected to raise error if no owner is set" do
+    expect { described_class.new }.to raise_error "An Account owner is required"
   end
 
-  it 'is expected to have an expiry date on initialize' do
-    expected_date = Date.today.next_year(5).strftime('%m/%y')
+  it "is expected to have an expiry date on initialize" do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
   end
 
-  it 'is expected to have :active status on initialize' do
+  it "is expected to have :active status on initialize" do
     expect(subject.account_status).to eq :active
   end
 
@@ -32,7 +32,7 @@ describe Account do
   #   expect(subject.account_status).to eq :deactivated
   # end
 
-  it 'deactivates account using the instance method' do
+  it "deactivates account using the instance method" do
     subject.deactivate
     expect(subject.account_status).to eq :deactivated
   end
